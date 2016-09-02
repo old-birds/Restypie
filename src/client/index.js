40391e111ed3aaa3a2baa27fd8a00cede46e5cec
Nicolas Gramlich <nicolas@luxe.com>
2016-09-01 20:17:57 -0700

    :sparkles: Added query method on client.

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const Query = require('./lib/query');
 const Restypie = require('../');
 
@@ -130,6 +131,24 @@ module.exports = class Client {
       url: this.url,
       headers: Object.assign({ 'Content-Type': 'application/json' }, this._defaultHeaders, params.headers)
     }).run().then(() => undefined);
+  }
+
+  query(method = Restypie.Methods.GET, path, filters, params) {
+    params = params || {};
+    return new Query({
+      method: method,
+      filters: filters,
+      limit: params.limit,
+      offset: params.offset,
+      sort: params.sort,
+      populate: params.populate,
+      options: params.options,
+      select: params.select,
+      url: path ? Restypie.Url.join(this.url, path) : this.url,
+      headers: Object.assign({ 'Accept': 'application/json' }, this._defaultHeaders, params.headers)
+    }).run().then((body) => {
+      return Promise.resolve(body.data);
+    });
   }
 
   count(filters, params) {
